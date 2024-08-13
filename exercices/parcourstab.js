3b5f8de634e00b1736fe57ce1dbf4e3034dbfec2
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-08-13 10:24:53 +0000

    pratique sur la boucle for

---
+++ exercices/parcourstab.js
@@ -0,0 +1,34 @@
+const tab =[1,2,3,6,7]
+for (let i = 0; i < tab.length; i++) {
+ tab[i];      
+}
+console.log(tab);
+
+
+//const types =[];
+//let i;
+
+//copier les elements du premier tableau dans le second
+
+ 
+
+const year =[1991, 2007, 1969 , 2012];
+ const curentYear =2024;
+const age =[];
+
+function calculAge(birthYear){
+         return curentYear - birthYear;
+}
+for (let i = 0; i < year.length; i++) {
+         age.push(calculAge(year[i]))
+         
+}
+console.log(age);
+
+
+//parcourir les elements du tableau spécifiquement
+
+const table=["ibrahim","ibrahim","ibrahim","ibrahim"]
+
+
+         
