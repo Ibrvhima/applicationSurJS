39d8a1576413feb1c898774abbb99a8a753dd8ea
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-08-13 11:22:15 +0000

    imbrication de for

--- exercices/parcourstab.js
+++ exercices/parcourstab.js
@@ -1,38 +1,43 @@
-const tab =[1,2,3,6,7]
+const tab = [1, 2, 3, 6, 7];
 for (let i = 0; i < tab.length; i++) {
- tab[i];      
+  tab[i];
 }
 console.log(tab);
-
 
 //const types =[];
 //let i;
 
 //copier les elements du premier tableau dans le second
 
- 
+const year = [1991, 2007, 1969, 2012];
+const curentYear = 2024;
+const age = [];
 
-const year =[1991, 2007, 1969 , 2012];
- const curentYear =2024;
-const age =[];
-
-function calculAge(birthYear){
-         return curentYear - birthYear;
+function calculAge(birthYear) {
+  return curentYear - birthYear;
 }
 for (let i = 0; i < year.length; i++) {
-         age.push(calculAge(year[i]))
-         
+  age.push(calculAge(year[i]));
 }
 console.log(age);
 
+//Retour de l'inverse d'un tableau
 
-//parcourir les elements du tableau spécifiquement
+const table = [1, 2, 3, 4, 5, 6, 7];
+for (let i = table.length - 1; i >= 0; i--) {
+  console.log(table[i]);
+}
 
-const table=[1,2,3,4,5,6,7];
-for (let i = table.length -1 ; i >= 0 ; i--) {
+//Imbrication de boucle
+
+for (let i = 1; i <= 5; i++) {
+         for (let j = 1; j <=5; j++) {
+                  console.log(`la repert${j} de l'exercice${i}`);
+                  
+         }
+         console.log(`la repet${i} de l'exercice${i}`);
          
-         console.log(table[i]);      
 }
 
 
-         
+
