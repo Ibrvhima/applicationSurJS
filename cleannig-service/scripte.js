7c3ffac4c60f89c9bc08a1dd6397a8bfcf83c054
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-09-02 12:14:55 +0000

    scope

---
+++ cleannig-service/scripte.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//scope global
+function calAge(birthYear) {
+  //scope calAge
+  console.log(firstName);
+
+  const age = 2024 - birthYear;
+  return age;
+  
+}
+
+const firstName = "Amadou"
+
+calAge(1990)
