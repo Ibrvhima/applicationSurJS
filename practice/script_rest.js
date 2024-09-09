47895b5b142f66629e7b78000aba1a798cd2adc8
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-09-09 19:07:18 +0900

    practice

--- practice/script_rest.js
+++ practice/script_rest.js
@@ -6,3 +6,10 @@ const heroescoffee = {
   startMenu: ["Salade cesar", "Nem", "Soupe au poisson"],
   mainMenu: ["Pizza Americaine", "Burger Heroes", "Maffe Poulet"],
 };
+//Operateur spread
+const heroescoffeeMatam = {...heroescoffee}
+console.log(heroescoffeeMatam);
+
+//The rest pattern
+const array =[1,2,3,4,5,8]
+array
