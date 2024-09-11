7a1dbe5548e84f72d0eeafd664081fc849786f75
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-09-11 11:32:58 +0000

    Iteration

--- practice/script_rest.js
+++ practice/script_rest.js
@@ -1,39 +1,63 @@
 "use strict";
+
+const weekdays = ["mon", "tue", "wed", "thursday", "fri", "sat", "sun"];
+
+const openinHours = {
+  [weekdays[3]]: {
+    open: 12,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // H24
+    close: 24,
+  },
+  mon: {},
+};
+
 const heroescoffee = {
   name: "Heroes Coffee",
   location: "Kipe Plaza, Ratoma Conakry Guinee",
   categories: ["Americaine", "Indienne", "Italienne", "Guineenne"],
   startMenu: ["Salade cesar", "Nem", "Soupe au poisson"],
   mainMenu: ["Pizza Americaine", "Burger Heroes", "Maffe Poulet"],
-
+  numGuests: null, // "" , false
+  openinHours, // 1/ Es6 enhance object literal
+  order(starterIdx, mainIdx) {
+    return [this.startMenu[starterIdx], this.mainMenu[mainIdx]];
+  },
+  orderDelivery({
+    time: deliveryTime = "20:00",
+    starterIdx = 1,
+    adress,
+    mainIdx = 1,
+    coursierId = 0,
+  }) {
+    console.log(`Order received! ${this.startMenu[starterIdx]} and ${this.mainMenu[mainIdx]} will be delivered to ${adress} at ${deliveryTime}`);
+  },
+  orderPizza: (ing1, ing2, ing3) =>
+    console.log(`Here is your delicious Pizza with ${ing1}, ${ing2}, ${ing3}`),
 };
-//Operateur spread
-const heroescoffeeMatam = {...heroescoffee}
-console.log(heroescoffeeMatam);
+heroescoffee.openinHours &&
+  heroescoffee.openinHours.mon &&
+  console.log(heroescoffee.openinHours.mon.open);
 
-//The rest pattern
-const array =[1,2,3,4,5,8]
-array
+if (heroescoffee.openinHours && heroescoffee.openinHours.mon)
+  console.log(heroescoffee.openinHours.mon.open);
 
-//BOUCLE FOR OF
-const menu = [...heroescoffee.mainMenu, ...heroescoffee.startMenu]
-for(const item of menu)
-  console.log(item);
- for(const item of menu.entries()){
-  console.log(item);
+console.log(heroescoffee.openinHours?.mon?.open);
 
+const days = ["mon", "tue", "wed", "thursday", "fri", "sat", "sun"];
+
+for (const day of days) {
+  console.log(day);
+  const open = heroescoffee.openinHours[day]?.open;
+  const close = heroescoffee.openinHours[day]?.close;
+
+  console.log(`on ${day} we ${
+      open ? `open in ${open} and close at ${close}` :"we are closed"
+      }
+      `);
 }
-
-const students =[
-  {nameStudent:"ousmane", age: 14},
-  {nameStudent:"fatou", age: 18},
-  {nameStudent:"oumar", age: 17},
-  {nameStudent:"Mamadou", age: 20},
-  {nameStudent:"Amadou", age: 14},
-]
-for(const items of students.entries()){
-  console.log(items);
-}
-
-
-
