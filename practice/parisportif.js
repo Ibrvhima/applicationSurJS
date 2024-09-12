f60020145a866bd8e85bc09f1996a53fb60cd3c9
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-09-12 05:53:41 +0000

    pari sportif

---
+++ practice/parisportif.js
@@ -0,0 +1,41 @@
+"use strict";
+const match = {
+  equipe1: "FC Barcelone",
+  equipe2: "Real Madrid",
+  joueurs: [
+    [
+      "Ter Stegen",
+      "Dest",
+      "Pique",
+      "Araujo",
+      "Alba",
+      "Busquets",
+      "De Jong",
+      "Pedri",
+      "Dembele",
+      "Depay",
+      "Fati",
+    ],
+    [
+      "Courtois",
+      "Carvajal",
+      "Militao",
+      "Alaba",
+      "Mendy",
+      "Casemiro",
+      "Kroos",
+      "Modric",
+      "Vinicius",
+      "Benzema",
+      "Rodrygo",
+    ],
+  ],
+  score: "3:2",
+  scorers: ["Fati", "Depay", "Fati", "Benzema", "Vinicius"],
+  date: "12 mars 2024",
+  odds: {
+    equipe1: 1.5,
+    x: 3.75,
+    equipe2: 5.0,
+  },
+};
