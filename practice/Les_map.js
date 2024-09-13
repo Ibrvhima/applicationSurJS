206113b4a995431c259cb6030e593a896c300a65
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-09-13 10:01:47 +0000

    another commit  on Map

---
+++ practice/Les_map.js
@@ -0,0 +1,11 @@
+const restaurant = new Map();
+
+restaurant.set("name", "heroes coffee")
+console.log(restaurant);
+
+//ajout d'un element dans un Map
+restaurant.set(1, "kipe Plazza")
+restaurant.set(2, "kipe T2")
+restaurant.set(3, "Kaloum")
+restaurant.set(4, "Begssia")
+console.log(restaurant);
