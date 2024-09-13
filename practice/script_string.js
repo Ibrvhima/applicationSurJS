4a7525edfe4f03a9303ead53e47a60cf4d450740
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-09-13 11:14:43 +0000

    Practice

--- practice/script_string.js
+++ practice/script_string.js
@@ -1,6 +1,6 @@
-const airline = "air Guinée"
+const airline = "air Guinée";
 
-const plane = "A320"
+const plane = "A320";
 
 console.log(plane[0]);
 console.log(plane[1]);
@@ -15,5 +15,37 @@ console.log(plane.length);
 console.log(plane.indexOf("3"));
 console.log(airline.indexOf("Guinée"));
 
+//********************************Application******************* */
+//### Exercice 1 : Extraction simple (Débutant)
+
+//Écrivez un programme qui prend une chaîne de caractères et affiche le premier mot de cette chaîne.
+//Utilisez `slice()` et `indexOf()` pour extraire le premier mot de la chaîne.
+
+const school = "Nimba Hub";
+console.log(school.indexOf());
+console.log(school.slice(0, 5));
+
+//Exercice 2 : Extraction avec fin dynamique (Intermédiaire)
+
+//Créez un programme qui extrait le dernier mot d'une chaîne, sans connaître sa longueur au préalable.
+//Utilisez `slice()` et `lastIndexOf()` pour extraire le dernier mot.
+console.log(school.slice(6));
 
 
+//### Exercice 3 : Vérification d'une position (Avancé)
+
+//Écrivez une fonction `checkMiddleSeat(seat)` qui prend en entrée un numéro de siège d'avion (par exemple '23B') et affiche si le siège est un siège du milieu (B ou E).
+
+//Utilisez `slice()` pour extraire la dernière lettre et vérifiez si c'est un 'B' ou un 'E'.
+
+function checkMiddleSeat(seat){
+         const checkeCaract = seat.slice(-1)
+         if (checkeCaract === "E" || checkeCaract === "B"){
+                  console.log("IS A MIDDEL SEAT");
+         }
+         else{
+                  console.log("IS NOT A MIDDEL SEAT");
+         }
+ 
+}
+checkMiddleSeat("23E")
