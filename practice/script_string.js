85421296e930b1561c4694149a912c736942e9e8
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-09-13 10:03:03 +0000

    script for strings

---
+++ practice/script_string.js
@@ -0,0 +1,19 @@
+const airline = "air Guinée"
+
+const plane = "A320"
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(plane[3]);
+
+console.log(plane.length);
+
+//Methode appliqué sur les string
+
+//<<IndexOf>> pour recuperer la position d'un caractère dans un string
+console.log(plane.indexOf("3"));
+console.log(airline.indexOf("Guinée"));
+
+
+
