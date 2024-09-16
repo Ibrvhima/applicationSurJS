39255cccfd020de42401ce010acffc98f01d2996
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-09-16 11:44:35 +0000

    new commit

--- practice/script_advencedfunction.js
+++ practice/script_advencedfunction.js
@@ -1,6 +1,6 @@
 "use Strict";
 
-const bookings = []
+const bookings = [];
 
 const createBooking = function (flightNum, numPassenger, price) {
   const booking = {
@@ -9,9 +9,28 @@ const createBooking = function (flightNum, numPassenger, price) {
     price,
   };
   console.log(booking);
-bookings.push(booking)
+  bookings.push(booking);
 };
 
-createBooking("A-777", "ID0604", "$400")
+createBooking("A-777", "ID0604", "$400");
 
+const flight = "B-707";
+const amad = {
+  namePassenger: "Amad Ly",
+  passeport: 2345678902,
+};
 
+const checkIn = function (flightNum, passenger) {
+  (flightNum = "BC-238"),
+    (passenger.namePassenger = "Mr. " + passenger.namePassenger);
+};
+
+checkIn(flight, amad);
+console.log(flight);
+console.log(amad);
+
+const newPasseport = function (person) {
+  person.passeport = Math.trunc(Math.random() * 100_000_000);
+};
+newPasseport(amad)
+checkIn(flight, amad)
