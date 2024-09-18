6e3aadef1e139508c8253145d78e8ad9ffe81de5
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-09-18 09:45:20 +0000

    closur suit

--- practice/script_closure.js
+++ practice/script_closure.js
@@ -14,3 +14,23 @@ const booker = securBooking();
 booker();
 booker();
 booker();
+
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+h();
+f()
