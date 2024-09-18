563c3ae232abb53172c065f14c908b8b6d1db25c
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2024-09-18 10:06:03 +0000

    closure suite

--- practice/script_closure.js
+++ practice/script_closure.js
@@ -29,8 +29,18 @@ const h = function () {
     console.log(b * 2);
   };
 };
-
-g();
-f();
 h();
 f()
+
+
+//exemple 2
+const boardPassenger = function(n, waitTime){
+  const perGroup = Math.floor(n/3)
+  setTimeout (function(){
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 group, each with ${perGroup} passengers`);
+  }, waitTime * 1000)
+console.log(`Will start boarding in ${waitTime} sec`);
+}
+
+boardPassenger(30,3)
